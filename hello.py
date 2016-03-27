38101a8078e9d7a9c3108c5d9d72e06930970ff1
Sung Park <sungwoo.park@students.olin.edu>
2016-03-26 22:06:46 -0400

    Implment all functions

--- hello.py
+++ hello.py
@@ -3,11 +3,19 @@ Simple "Hello, World" application using Flask
 """
 
 from flask import Flask
+from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/login/', methods=['GET','POST'])
+def login(name=None, age=None, patrick=None):
+    return render_template('login.html', name=request.form['name'], age=request.form['age'], patrick=request.form['patrick'])
 
 if __name__ == '__main__':
+    #app.debug = True
     app.run()
