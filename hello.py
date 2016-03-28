a2167c05d5b21c865965c53e538c8f60d8167b28
willem.thorbecke@students.olin.edu <willem.thorbecke@students.olin.edu>
2016-03-28 01:21:19 -0400

    done

--- hello.py
+++ hello.py
@@ -3,11 +3,20 @@ Simple "Hello, World" application using Flask
 """
 
 from flask import Flask
+from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+    
+@app.route('/answers/', methods=['GET','POST'])
+def answers(name=None, age=None, fav=None):
+    return render_template('answers.html', name=request.form['name'], age=request.form['age'], fav=request.form['fav'])
+
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
